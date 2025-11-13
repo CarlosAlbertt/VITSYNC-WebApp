7c7da678d2c8eeaf7e9e203ca12374950c7c00be
CarlosAlbertt <caralbort3@alu.edu.gva.es>
2025-11-13 20:23:39 +0100

    Borrado contenido de App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,6 @@ const mensaje = ref("Hola Mundo desde Vue 3!");
 
 <template>
   <main>
-    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 mt-10 border border-indigo-200">
-      <div class="flex-shrink-0">
-        <!-- Icono simple (emoji o SVG inline) -->
-        <span class="text-4xl">👋</span>
-      </div>
-      <div>
-        <div class="text-xl font-medium text-indigo-600">Aplicación Vue</div>
-        <!-- Mostramos la variable reactiva -->
-        <p class="text-gray-500">{{ mensaje }}</p>
-      </div>
-    </div>
     <router-view />
   </main>
 </template>
